7bda60f18a1a4b5f8df0104f605c5c98d05cbbe4
NS\alex.v <alex.v@nsysgroup.com>
2021-12-13 12:41:53 +0300

    Fix

--- index.py
+++ index.py
@@ -9,9 +9,9 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header("Transfer-Encoding", "chunked")
         self.end_headers()
 
-        chunks = ['a' * 10, 'b' * 11, 'c' * 12]
+        arr = ['a' * 10, 'b' * 11, 'c' * 12]
         
-        for chunkItem in chunks:
-            self.wfile.write(b"%x\r\n%s\r\n" % (len(chunkItem), bytes(chunkItem, 'UTF-8')))
+        for item in arr:
+            self.wfile.write(b"%x\r\n%s\r\n" % (len(item), bytes(item, 'UTF-8')))
 
 with HTTPServer(('', 8080), RequestHandler) as server: server.serve_forever()
